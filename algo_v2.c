9b1d929b254676a0df5ef5d4fd81c7beb7a03af6
ltressen <paloouf@gmail.com>
2023-04-20 16:53:18 +0200

    on est presque bon

--- algo_v2.c
+++ algo_v2.c
@@ -6,48 +6,11 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 13:10:33 by ltressen          #+#    #+#             */
-/*   Updated: 2023/04/19 15:53:35 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/04/20 16:49:41 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-/*void	best_choice(t_stack *st, int i, float n, float m)
-{
-	int	j;
-
-	j = i;
-	if (st->len <= 10)
-	{
-		algo_one(st);
-	}
-	if ((st->a[i] >= st->o[(int)(st->len * n)] && st->a[i] < st->o[(int)(st->len * (n+0.1))]) || (st->a[i] >= st->o[(int)(st->len * m)] && st->a[i] <= st->o[(int)(st->len * (m+0.1) - 1)]))
-	{
-		if (i <= st->index/2)
-		{
-			//tant qu'on est pas revenu au debut
-			while(j >= 1)
-			{
-				rotate_a(st, 0);
-				j--;
-			}
-		}
-		else
-		{
-			//tant qu'on est pas arrive a la fin
-			while (j <= st->index - 1)
-			{
-				r_rotate_a(st, 0);
-				j++;
-			}
-		}
-		push_b(st);
-		if (st->index < st->len - 1)
-			algo_v2_0_2(st, n);
-	}
-}*/
-
-
 
 void	on_fait_des_essais(t_stack *st, int n, int m)
 {
@@ -66,7 +29,6 @@ void	on_fait_des_essais(t_stack *st, int n, int m)
 			j = i;
 		i++;
 	}
-	//tant qu'on est pas revenu au debut
 	if (j >= 0)
 	{
 		while(j >= 1)
@@ -78,50 +40,9 @@ void	on_fait_des_essais(t_stack *st, int n, int m)
 		push_b(st);
 	}
 	if (st->index < st->len - 1)
-		algo_v2_0_2(st, n);
+		algo_v2_0_2(st, n, m);
 
 }
-
-
-/*void	closest_index(t_stack *st, float n, float m)
-{
-	int i;
-	int j;
-	int index_start;
-	int index_end;
-
-	j = st->index - 1;
-	i = -1;
-	index_start = -1;
-	index_end = -1;
-	//printf("n : %f , m : %f \n", n,m);
-	while (index_start < 0 && i < st->index)
-	{
-		i++;
-		if ((st->a[i] >= st->o[(int)(st->len * n)] && st->a[i] < st->o[(int)(st->len * (n+0.1))]) || (st->a[i] >= st->o[(int)(st->len * m)] && st->a[i] <= st->o[(int)(st->len * (m+0.1) - 1)]))
-		{
-			index_start = i;	
-			ft_printf("%d \t", index_start);
-		}
-	}
-	ft_printf("%d , %d ", i, j);
-	while (index_end < 0 && j > 0)
-	{
-		if ((st->a[j] >= st->o[(int)(st->len * n)] && st->a[j] < st->o[(int)(st->len * (n+0.1))]) || (st->a[j] >= st->o[(int)(st->len * m)] && st->a[j] <= st->o[(int)(st->len * (m+0.1) - 1)]))
-		{
-			index_end = st->index - j;
-			ft_printf("%d \t", index_end);
-		}
-		j--;
-	}
-	if (index_start > index_end)
-	{
-		//le meilleur move est rra
-		best_choice(st, j + 1, n, m);
-	}
-	else
-		best_choice(st, i, n, m);
-}*/
 
 int	is_p_in_b(t_stack *st, int n, int m)
 {
@@ -152,7 +73,7 @@ int	is_p_in_a_sorted(t_stack *st, int n, int m)
 
 	i = 0;
 	check = 0;
-	if (st->index < st->len)
+	if (st->index <= st->len)
 	{
 		while (st->a[i])
 		{
@@ -167,69 +88,140 @@ int	is_p_in_a_sorted(t_stack *st, int n, int m)
 	return (0);
 }
 
-//pre-tri 1/10 dans l'algo de tri pour push dans b
 void	algo_v2_0_1(t_stack *st)
 {
 	while (!is_p_in_b(st, st->min, st->p2))
-	{
-		//closest_index(st, n, m);
 		on_fait_des_essais(st, st->min, st->p2);
-	}	
 	while (!is_p_in_b(st, st->p2, st->p4))
-	{
-		//closest_index(st, n, m);
 		on_fait_des_essais(st, st->p2, st->p4);
-	}	
 	while (!is_p_in_b(st, st->p4, st->p6))
-	{
-		//closest_index(st, n, m);
 		on_fait_des_essais(st, st->p4, st->p6);
-	}
 	while (!is_p_in_b(st, st->p6, st->p8))
-	{
-		//closest_index(st, n, m);
 		on_fait_des_essais(st, st->p6, st->p8);
-	}	
 	while (!is_p_in_b(st, st->p8, st->max))
-	{
-		//closest_index(st, n, m);
 		on_fait_des_essais(st, st->p8, st->max);
-	}
-	now_we_sort(st);
+	now_we_sort(st, st->p9, st->max);
+	now_we_sort_more(st, st->b[find_big(st)], st->b[find_small(st, st->p8, st->p9)]);
+	now_we_sort(st, st->p8, st->p9);
+	now_we_sort_better(st);
+	now_we_sort(st, st->p7, st->p8);
+	now_we_sort_more(st, st->b[find_big(st)], st->b[find_small(st, st->p6, st->p7)]);
+	now_we_sort(st, st->p6, st->p7);
+	now_we_sort_better(st);
+	now_we_sort(st, st->mid, st->p6);
+	now_we_sort_more(st, st->b[find_big(st)], st->b[find_small(st, st->p4, st->mid)]);
+	now_we_sort(st, st->p4, st->mid);
+	now_we_sort_better(st);
+	now_we_sort(st, st->p3, st->p4);
+	now_we_sort_more(st, st->b[find_big(st)], st->b[find_small(st, st->p2, st->p3)]);
+	now_we_sort(st, st->p2, st->p3);
+	now_we_sort_better(st);
+	now_we_sort(st, st->p1, st->p2);
+	now_we_sort_more(st, st->b[find_big(st)], st->b[find_small(st, 0, st->p1)]);
+	now_we_sort(st, 0, st->p1);
+	while (st->a[0] != st->o[0])
+		r_rotate_a(st, 0);
 }
 
-void	now_we_sort(t_stack *st)
+void	now_we_sort(t_stack *st, int n, int m)
 {
+	int	big;
+	int	small;
 	int	i;
-	int	lol;
 
 	i = 0;
-	while (!is_p_in_a_sorted(st, st->p9, st->max))
+	while (!is_p_in_a_sorted(st, n, m))
 	{
-		if (st->b[i] >= st->p9 && st->b[i] < st->max)
+		if (st->b[i] >= st->o[n] && st->b[i] < st->o[m])
 		{
-			lol = find_big(st);
-			if (lol < ((st->len - st->index -1) /2))
+			big = find_big(st);
+			small = find_small(st, n, m);
+			if (big < small)
 			{
-				while (lol > 0)
+				if (big < ((st->len - st->index -1) /2))
 				{
-					rotate_b(st, 0);
-					lol--;
+					while (big > 0)
+					{
+						rotate_b(st, 0);
+						big--;
+					}
+					push_a(st);
 				}
-				push_a(st);
+				else
+				{
+					while (big <= (st->len - st->index -1))
+					{
+						r_rotate_b(st, 0);
+						big++;
+					}
+					push_a(st);
+				}
 			}
 			else
 			{
-				while (lol <= (st->len - st->index -1))
+				if (small < ((st->len - st->index -1) /2))
 				{
-					r_rotate_b(st, 0);
-					lol++;
+					while (small > 0)
+					{
+						rotate_b(st, 0);
+						small--;
+					}
+					push_a(st);
+					if (st->index != st->len)
+						rotate_a(st, 0);
 				}
-				push_a(st);
+				else
+				{
+					while (small <= (st->len - st->index -1))
+					{
+						r_rotate_b(st, 0);
+						small++;
+					}
+					push_a(st);
+					if (st->index != st->len)
+						rotate_a(st, 0);
+				}
 			}
 		}
 		i++;
 	}
+}
+
+void	now_we_sort_better(t_stack *st)
+{
+	while (!is_a_sorted(st))
+		r_rotate_a(st, 0);
+}
+
+void	now_we_sort_more(t_stack *st, int big, int small)
+{
+	while (!is_a_sorted(st))
+	{
+		if (st->b[0] != big && st->b[0] != small)
+			r_ra_rb(st);
+		else
+			r_rotate_a(st, 0);
+	}
+}
+
+int	find_small(t_stack *st, int n, int m)
+{
+	int	i;
+	int	small;
+	int	index;
+
+	i = 0;
+	small = INT32_MAX;
+	while(st->b[i])
+	{
+		if ((st->b[i] >= st->o[n] && st->b[i] < st->o[m]) && st->b[i]< small)
+		{
+			index = i;
+			small = st->b[i];
+		}
+		i++;
+	}
+	return (index);
 }
 
 int	find_big(t_stack *st)
@@ -252,7 +244,7 @@ int	find_big(t_stack *st)
 	return (index);
 }
 
-void	algo_v2_0_2(t_stack *st, int n)
+void	algo_v2_0_2(t_stack *st, int n, int t)
 {
 	int	m;
 	if (n == st->min)
@@ -266,5 +258,10 @@ void	algo_v2_0_2(t_stack *st, int n)
 	else if (n == st->p8)
 		m = st->p9;
 	if ((st->b[0] >= st->o[n] && st->b[0] < st->o[m]))
-		rotate_b(st, 0);
+	{
+		if (st->a[0]  >= st->o[n] && st->a[0] < st->o[t])
+			rotate_b(st, 0);
+		else
+			ra_rb(st);
+	}
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 09:18:16 by ltressen          #+#    #+#             */
-/*   Updated: 2023/04/19 15:34:07 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/04/20 16:01:48 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,14 +59,17 @@ void	algo_three(t_stack *st);
 int	is_b_sorted(t_stack *st);
 int	is_a_sorted(t_stack *st);
 void	algo_v2_0_1(t_stack *st);
-void	algo_v2_0_2(t_stack *st, int n);
+void	algo_v2_0_2(t_stack *st, int n, int t);
 int	is_p_in_b(t_stack *st, int n, int m);
 int	is_p_in_a_sorted(t_stack *st, int n, int m);
 void	best_choice(t_stack *st, int i, float n, float m);
 void	closest_index(t_stack *st, float n, float m);
 void	on_fait_des_essais(t_stack *st, int n, int m);
 int	find_big(t_stack *st);
-void	now_we_sort(t_stack *st);
+void	now_we_sort(t_stack *st, int n, int m);
+int	find_small(t_stack *st, int n, int m);
+void	now_we_sort_more(t_stack *st, int big, int small);
+void	now_we_sort_better(t_stack *st);
 //void	quicksort(t_stack *st, int low, int high);
 //int	partition(t_stack *st, int low, int high);
 
